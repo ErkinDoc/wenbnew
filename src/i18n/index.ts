4c2af132d29079ab716f0e3224ce9c39739f6092
ErkinDoc <erkinslovakia@gmail.com>
2026-03-08 12:41:51 +0100

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,4 @@
-import in18 from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import ru from './locales/ru.json';
