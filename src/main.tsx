cebeb848d9dbf2d2fe04d9a1acb8c93c77395b0a
ErkinDoc <erkinslovakia@gmail.com>
2026-03-08 10:31:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 // Force rebuild 2026-02-14
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  console.error('Failed to find the root element. Check your index.html');
+} else {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
