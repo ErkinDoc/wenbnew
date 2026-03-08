7d8eb4bbf35165a76c364f6a0f368cdb3b033516
ErkinDoc <erkinslovakia@gmail.com>
2026-03-08 12:39:59 +0100

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,25 @@
+import in18 from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import ru from './locales/ru.json';
 import en from './locales/en.json';
 import sk from './locales/sk.json';
 
-export const locales = {
+const resources = {
   ru: { translation: ru },
   en: { translation: en },
   sk: { translation: sk }
 };
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'sk',
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
